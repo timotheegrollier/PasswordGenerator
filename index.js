324ef62711ef259a927cabea4586d716c16fbe60
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-04-13 11:03:08 +0200

    V0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "!:;,?./§ù^$*µ£¨%=)àç_è-(é&";
+
+const generatePassword = () => {
+  let data = [];
+  if (lowercase.checked) data.push(dataLowercase);
+  else if (uppercase.checked) data.push(dataUppercase);
+  console.log(data);
+};
+
+generatePassword();
+
+// event listener sur le click
